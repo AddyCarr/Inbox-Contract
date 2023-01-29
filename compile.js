1bf223004f7052eefb7cf1e9c7fdd85274c66922
Adam Carr <addycarrr@gmail.com>
2023-01-29 13:42:25 -0500

    Module requireing

--- compile.js
+++ compile.js
@@ -6,7 +6,7 @@ const inboxPath = path.resolve(__dirname, "contracts", "inbox.sol");
 
 const source = fs.readFileSync(inboxPath, "utf8");
 
-console.log(solc.compile(source, 1));
+module.exports = solc.compile(source, 1).contracts[":Inbox"];
 
 //QUESTIONS
 //Research: Solc, require statement
