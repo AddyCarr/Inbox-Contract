6d4c3ac31ed966031fd0ce6ddf9beb9c08779990
Adam Carr <addycarrr@gmail.com>
2023-01-31 13:38:26 -0500

    create provider for ganache

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,3 +1,5 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
+//creates an instance of web3 connected to teh ganache local network using a provider (neccesary com tool)
+const web3 = new Web3(ganache.provider());
