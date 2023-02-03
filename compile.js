b2381bb744f9b9e5c80e73712e3115930223447b
Adam Carr <addycarrr@gmail.com>
2023-02-03 11:18:29 -0500

    compiler edit

--- compile.js
+++ compile.js
@@ -6,6 +6,7 @@ const inboxPath = path.resolve(__dirname, "contracts", "inbox.sol");
 
 const source = fs.readFileSync(inboxPath, "utf8");
 
+// "1" to compile one contract
 module.exports = solc.compile(source, 1).contracts[":Inbox"];
 
 //QUESTIONS
