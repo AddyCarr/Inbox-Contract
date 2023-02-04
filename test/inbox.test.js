d5ad010eaca1064d7d50bda1d04a0a244f134bda
Adam Carr <addycarrr@gmail.com>
2023-02-04 17:12:53 -0500

    deploy and send interface

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,52 +3,24 @@ const ganache = require("ganache");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 
-beforeEach(() => {
+let accounts;
+let inbox;
+//to get access to a variable stored within function scope of before each, declare beforehand
+
+beforeEach(async () => {
   // get a list of all accounts
-  web3.eth.getAccounts().then((fetchedAccounts) => {
-    console.log(fetchedAccounts);
-  });
+  accounts = await web3.eth.getAccounts();
+  // async is marked after function. await keyword replaced .then
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: Bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
-  it("deploys a contract", () => {});
+  it("deploys a contract", () => {
+    console.log(inbox);
+  });
 });
-
-// just about, everamacyion we
-// CAR EXAMPLE MOCHA TEST
-// ? What is a class and it's properties
-// class Car {
-//   park() {
-//     return "stopped";
-//   }
-
-//   drive() {
-//     return "vroom";
-//   }
-// }
-
-//"carr" below is not related to class above, just an arbitray name. All values in strings are not related variables.
-//arrow function contains all of the it statements
-// remmebr function scope of vriables is only within  the funcation or neste functions.
-// thi s is why we must initialise car outside of beforeEacj statement.
-
-// let car;
-// //let bc we are reassigning value in ea fnc.
-
-// beforeEach(() => {
-//   car = new Car();
-// });
-
-// describe("Car", () => {
-//   it("can park", () => {
-//     const car = new Car();
-//     assert.equal(car.park(), "stopped");
-//   });
-
-//   it("can drive", () => {
-//     const car = new Car();
-//     assert.equal(car.drive, "vroom");
-//   });
-// });
-
-// //above is a mocha structure where two tests are nested ina  describe statement, where the 'it' statement tests are being tested for teh assertations of the test, vroom and drive.
