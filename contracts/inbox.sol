1bdf4b6fdc24d83d341a24ce7baa66a4c3647676
Adam Carr <addycarrr@gmail.com>
2023-02-15 15:22:42 -0500

    Licence and NatSpec update

--- contracts/inbox.sol
+++ contracts/inbox.sol
@@ -1,6 +1,12 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.4.17;
 
-//contract definition
+
+/// @title Inbox storage and retreival contract
+/// @dev This contract is used to store and retreive a message
+/// @author Adam Carr
+/// @notice You can use this contract for only the most basic simulation
+
 contract Inbox {
     string public message;
 
